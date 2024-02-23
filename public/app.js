9424bdff729368c3a2e70ab354bc001cbe853a0d
Aleksey <alekseycrosso@gmail.com>
2024-02-23 13:29:39 +0300

    add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -16,5 +16,24 @@ function add_to_cart(id)
     var x = window.localStorage.getItem(key);
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
+
+    alert('Items in your cart: ' + cart_get_number_of_items());
 }
 
+function cart_get_number_of_items()
+{
+    var count = 0;
+
+    for(var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i); // получаем ключ
+        var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+        if(key.indexOf('product_') == 0)
+        {
+            count += value * 1;
+        }
+    }
+
+    return count;
+}
