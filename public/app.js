ade8fb8089cd14d0fc65539c95d614365eb969a4
Aleksey <alekseycrosso@gmail.com>
2024-02-24 19:46:10 +0300

    update orders input field

--- public/app.js
+++ public/app.js
@@ -17,6 +17,8 @@ function add_to_cart(id)
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
 
+    update_orders_input();
+
     alert('Items in your cart: ' + cart_get_number_of_items());
 }
 
@@ -36,4 +38,27 @@ function cart_get_number_of_items()
     }
 
     return count;
+}
+function update_orders_input()
+{
+    var orders = cart_get_orders();
+    $('#orders_input').val(orders);
+}
+
+function cart_get_orders()
+{
+    var orders = '';
+
+    for(var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i); // получаем ключ
+        var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+        if(key.indexOf('product_') == 0)
+        {
+            orders = orders + key + '=' + value + ',';
+        }
+    }
+
+    return orders;
 }
